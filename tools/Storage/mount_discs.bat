1cc41b7b103f9768fd93048f149728272747c588
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2019-05-29 10:36:28 +0200

    Update mount script.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- tools/Storage/mount_discs.bat
+++ tools/Storage/mount_discs.bat
@@ -38,20 +38,18 @@ tools\TegraRcmSmash\TegraRcmSmash.exe -w tools\memloader\memloader_usb.bin --dat
 echo.
 echo Le disque devrait être monté sur votre système. Pour le démonter, éjecter le périphérique à l'aide du bouton "retirer le périphérique en toute sécurité" situé sur la barre des tâches en bas à droite puis forcer l'extinction de la Switch en maintenant le bouton POWER pendant 10 secondes (Attention à ne pas écrire/lire de données pendant cette opération sous peine d'endommager gravement les données de votre nand/sd).
 IF "%disc_mounted%"=="1" echo Pour explorer la mémoire interne de la Switch vous devez utiliser l'outil HacDiskMount lancé en tant qu'administrateur (nécessite d'avoir les biskey pour décrypter les données mais non nécessaire pour faire un dump de la nand). Si vous souhaitez faire un dump de la nand via cette méthode, le dump peut prendre du temps (environ trois heures).
-IF "%disc_mounted%"=="3" echo Pour explorer la partition boot0 de la Switch vous devez utiliser l'outil HacDiskMount lancé en tant qu'administrateur (nécessite d'avoir les biskey pour décrypter les données mais non nécessaire pour faire un dump de la partition). Si vous souhaitez faire un dump de la partition via cette méthode, le dump peut prendre du temps.
-IF "%disc_mounted%"=="4" echo Pour explorer la partition boot1 de la Switch vous devez utiliser l'outil HacDiskMount lancé en tant qu'administrateur (nécessite d'avoir les biskey pour décrypter les données mais non nécessaire pour faire un dump de la partition). Si vous souhaitez faire un dump de la partition via cette méthode, le dump peut prendre du temps.
 echo Parfois, le disque n'est pas reconu automatiquement. Vous devez ouvrir le gestionnaire de périphérique, trouver le périphérique avec un point d'exclamation nommé "Périphérique d’entrée USB" (testé sous Windows 7), faire un clique droit dessus, cliquer sur "Mettre à jour le pilote...", cliquer sur "Rechercher automatiquement un pilote mis à jour" puis cliquer sur "Fermer". Le périphérique devrait maintenant être utilisable.
 set /p launch_devices_manager=Souhaitez-vous lancer le gestionnaire de périphérique? (o/n): 
 echo.
 IF NOT "%launch_devices_manager%"=="" set launch_devices_manager=%launch_devices_manager:~0,1%
 IF /i "%launch_devices_manager%"=="o" start devmgmt.msc
-IF %disc_mounted% NEQ 2 (
+IF %disc_mounted% EQU 2 (
 	set /p launch_hacdiskmount=Souhaitez-vous lancer HacDiskMount? (o/n^): 
 )
-	IF %disc_mounted% NEQ 2 (
+	IF %disc_mounted% EQU 1 (
 	IF NOT "%launch_hacdiskmount%"=="" set launch_hacdiskmount=%launch_hacdiskmount:~0,1%
 )
-IF %disc_mounted% NEQ 2 (
+IF %disc_mounted% EQU 1 (
 	IF /i "%launch_hacdiskmount%"=="o" start tools\HacDiskMount/HacDiskMount.exe
 )
 :end_script
