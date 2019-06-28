667a01fe0aaa104954ba0717625119260cf08e14
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2019-06-28 21:24:14 +0200

    Continuing new Update Manager developement.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

--- tools/Storage/functions/update_manager.bat
+++ tools/Storage/functions/update_manager.bat
@@ -3,11 +3,7 @@ Setlocal enabledelayedexpansion
 @echo off
 chcp 65001 >nul
 echo é >nul
-IF EXIST templogs (
-	del /q templogs 2>nul
-	rmdir /s /q templogs 2>nul
-)
-mkdir templogs
+rem IF "%~2"=="forced" set verif_update=Y
 set base_script_path="%~dp0\..\..\.."
 set folders_url_project_base=https://github.com/shadow2560/Ultimate-Switch-Hack-Script/trunk
 set files_url_project_base=https://raw.githubusercontent.com/shadow2560/Ultimate-Switch-Hack-Script/master
@@ -16,6 +12,24 @@ IF NOT "%~nx0"=="update_manager_tmp.bat" (
 ) else (
 	cd /d "%base_script_path%"
 )
+IF EXIST templogs (
+	del /q templogs 2>nul
+	rmdir /s /q templogs 2>nul
+)
+mkdir templogs
+IF EXIST failed_updates (
+	del /q failed_updates 2>nul
+	rmdir /s /q failed_updates 2>nul
+)
+mkdir failed_updates
+:failed_updates_verification
+IF NOT EXIST "failed_updates\*.failed" goto:skip_failed_updates_verification
+cd "failed_updates"
+for %%f in (*.failed) do (
+	call :update_failed_content "%%f"
+)
+cd ..
+:skip_failed_updates_verification
 :general_content_update
 
 :skip_general_content_update
@@ -28,33 +42,81 @@ IF "%~1"=="" (
 rem Specific scripts instructions must be added here
 
 :verif_file_version
-set temp_file_slash_path=%~1
-set temp_file_slash_path=%temp_file_slash_path:\=/%
+set temp_file_path=%~1
+set temp_file_slash_path=%temp_file_path:\=/%
+ping /n 2 www.google.com >nul 2>&1
+IF %errorlevel% NEQ 0 (
+	echo Aucune connexion internet vérifiable, la vérification de version du fichier "%temp_file_path%" n'aura pas lieu.
+	exit /b 0
+)
 call :test_write_access file "%~1"
 set /p script_version=<"%~1.version"
 "tools\gnuwin32\bin\wget.exe" --no-check-certificate --content-disposition -S -O "templogs\version.txt" %files_url_project_base%/%temp_file_slash_path%.version
 set /p script_version_verif=<"templogs\version.txt"
 call :compare_version
-exit /b
+exit /b %errorlevel%
 
 :verif_folder_version
-set temp_folder_slash_path=%~1
-set temp_folder_slash_path=%temp_folder_slash_path:\=/%
+set temp_folder_path=%~1
+set temp_folder_slash_path=%temp_folder_path:\=/%
+ping /n 2 www.google.com >nul 2>&1
+IF %errorlevel% NEQ 0 (
+	echo Aucune connexion internet vérifiable, la vérification de version du dossier "%temp_folder_path%" n'aura pas lieu.
+	exit /b 0
+)
 call :test_write_access folder "%~1"
 set /p script_version=<"%~1\folder_version.txt"
 "tools\gnuwin32\bin\wget.exe" --no-check-certificate --content-disposition -S -O "templogs\version.txt" %files_url_project_base%/%temp_folder_slash_path%/folder_version.txt
 set /p script_version_verif=<"templogs\version.txt"
 call :compare_version
-exit /b
+exit /b %errorlevel%
 
 :update_file
-"tools\gnuwin32\bin\wget.exe" --no-check-certificate --content-disposition -S -O "%temp_file_slash_path:/=\%" %files_url_project_base%/%temp_file_slash_path%
-"tools\gnuwin32\bin\wget.exe" --no-check-certificate --content-disposition -S -O "%temp_file_slash_path:/=\%.version" %files_url_project_base%/%temp_file_slash_path%.version
+ping /n 2 www.google.com >nul 2>&1
+IF %errorlevel% NEQ 0 (
+	echo Aucune connexion internet vérifiable, la mise à jour du fichier "%temp_file_path%" n'aura pas lieu.
+	exit /b 404
+)
+echo %temp_file_path%>"failed_updates\%temp_file_path:\=;%.file.failed"
+"tools\gnuwin32\bin\wget.exe" --no-check-certificate --content-disposition -S -O "%temp_file_path%" %files_url_project_base%/%temp_file_slash_path%
+IF %errorlevel% NEQ 0 (
+	echo Erreur lors de la mise à jour du fichier "%temp_file_path%", le script va se fermer pour pouvoir relancer le processus de mise à jour lors du prochain redémarrage de celui-ci.
+	IF EXIST templogs (
+		rmdir /s /q templogs
+	)
+	pause
+	exit
+)
+"tools\gnuwin32\bin\wget.exe" --no-check-certificate --content-disposition -S -O "%temp_file_path%.version" %files_url_project_base%/%temp_file_slash_path%.version
+IF %errorlevel% NEQ 0 (
+	echo Erreur lors de la mise à jour du fichier "%temp_file_path%.version", le script va se fermer pour pouvoir relancer le processus de mise à jour lors du prochain redémarrage de celui-ci.
+	IF EXIST templogs (
+		rmdir /s /q templogs
+	)
+	pause
+	exit
+)
+del /q "failed_updates\%temp_file_path:\=;%.file.failed"
 exit /b
 
 :update_folder
-rmdir /s /q "%temp_folder_slash_path:/=\%"
-"tools\gitget\SVN\svn.exe" export %folder_url_project_base%/%temp_folder_slash_path:/=\% "%temp_folder_slash_path:/=\%" --force
+ping /n 2 www.google.com >nul 2>&1
+IF %errorlevel% NEQ 0 (
+	echo Aucune connexion internet vérifiable, la mise à jour du dossier "%temp_folder_path%" n'aura pas lieu.
+	exit /b 404
+)
+echo %temp_folder_path%>"failed_updates\%temp_folder_path:\=;%.fold.failed"
+rmdir /s /q "%temp_folder_path%"
+"tools\gitget\SVN\svn.exe" export %folder_url_project_base%/%temp_folder_slash_path% "%temp_folder_path%" --force
+IF %errorlevel% NEQ 0 (
+	echo Erreur lors de la mise à jour du dossier "%temp_folder_path%", le script va se fermer pour pouvoir relancer le processus de mise à jour lors du prochain redémarrage de celui-ci.
+	IF EXIST templogs (
+		rmdir /s /q templogs
+	)
+	pause
+	exit
+)
+del /q "failed_updates\%temp_folder_path:\=;%.folder.failed"
 exit /b
 
 :compare_version
@@ -98,10 +160,27 @@ IF "%~1"=="folder" (
 )
 exit /b
 
+:update_failed_content
+set temp_failed_update_path=<"%~1"
+set temp_failed_file=%~1
+IF "%temp_failed_file:~-11,4%"=="file" (
+	set temp_file__path=%temp_failed_update_path%
+	set temp_file_slash_path=%temp_failed_update_path:\=/%
+	call :update_file
+)
+IF "%temp_failed_file:~-11,4%"=="fold" (
+	set temp_folder_path=%temp_failed_update_path%
+	set temp_folder_slash_path=%temp_failed_update_path:\=/%
+	call :update_folder
+)
+exit /b
+
 :end_script
 IF EXIST templogs (
 	rmdir /s /q templogs
-	mkdir templogs
+)
+IF NOT EXIST "failed_updates\*.failed" (
+	rmdir /s /q failed_updates
 )
 cls
 endlocal
