beae34d42f99562596df16fdc55c21a6f3dd1430
shadow2560 <24191064+shadow2560@users.noreply.github.com>
2019-08-03 12:54:31 +0200

    Update languages.
    Signed-off-by: shadow2560 <24191064+shadow2560@users.noreply.github.com>

---
+++ languages/EN_us/tools/Storage/install_nsp_USB.bat
@@ -0,0 +1,10 @@
+goto:%~1
+
+:display_title
+title NSP USB install %this_script_version% - Shadow256 Ultimate Switch Hack Script %ushs_version%
+goto:eof
+
+:intro
+echo This script will install NSPs on a USB connected Switch with Goldleaf.
+echo Goldleaf should be launched in USB mode, the console need to be connected via USB and the drivers should be installed.
+goto:eof
